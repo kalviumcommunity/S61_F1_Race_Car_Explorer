9d4b0356f34081b737138eaa07bc9af613562d7a
Ishana0 <ishanakalikiri01@gmailcom>
2024-04-02 10:39:40 +0530

    port

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ App.get("/", (req, res) => {
 App.get("/ping", (req, res) => {
     res.send("pong")
 });
-const port = 3000;
+
+App.use((err, req, res, next) => {
+    console.error("An error occurred", err)
+    res.status(500).json({error: "Something went wrong.."})
+})
+const port = process.env.PORT || 3001;
 
 App.listen(port, () => {
     console.log(`server is running on port ${port}`)
