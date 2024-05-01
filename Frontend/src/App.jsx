80a28cbdedb24d8ba990f7dac1e61729d57405e2
Ishana0 <ishanakalikiri01@gmailcom>
2024-05-01 13:27:57 +0530

    entity

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,30 +1,17 @@
+import React from 'react';
 import './App.css';
 import LandPage from './Components/LandPage';
 import Entity from './Components/Entity';
+import entityData from './Components/entityData.json';
 
-const App = () => {
-  const entities = [
-    {
-      name: "Lewis Hamilton",
-      team: "Mercedes-AMG Petronas Formula One Team",
-      carModel: "Mercedes-AMG F1 W13 E Performance",
-      engine: "Mercedes-AMG F1 M13 E Performance",
-      winsIn2023Season: 8,
-      polePositionsIn2023Season: 10
-    },
-    
-  ];
-
+function App() {
   return (
     <>
       <LandPage />
-      <div>
-        <h1>Formula 1 Entities</h1>
-        <div>
-          {entities.map((entity, index) => (
-            <Entity key={index} entity={entity} />
-          ))}
-        </div>
+      <div className="entity-list">
+        {entityData.map((entity, index) => (
+          <Entity key={index} {...entity} />
+        ))}
       </div>
     </>
   );
