995a3b627fff128cb20e17aef02cd4128b1713d2
Ishana0 <ishanakalikiri01@gmailcom>
2024-05-02 13:39:49 +0530

    render the data in the entity component

--- Frontend/src/Components/Entity.jsx
+++ Frontend/src/Components/Entity.jsx
@@ -1,16 +1,38 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
 
-const Entity = (props) => {
-  const { id, name, team, carModel, engine, winsIn2023Season, polePositionsIn2023Season } = props;
+const Entity = () => {
+  const [entities, setEntities] = useState(null);
+
+  useEffect(() => {
+    // Fetch entities data from the server
+    axios.get('http://localhost:3000/api/racecars')
+      .then(response => {
+        console.log(response.data);
+        setEntities(response.data.raceCars);
+      })
+      .catch(error => {
+        console.error('Error fetching entities:', error);
+        setEntities([]); // Set entities to an empty array if there's an error
+      });
+  }, []);
 
   return (
-    <div className="entity">
-      <h2>{name}</h2>
-      <p>Team: {team}</p>
-      <p>Car Model: {carModel}</p>
-      <p>Engine: {engine}</p>
-      <p>Wins in 2023 Season: {winsIn2023Season}</p>
-      <p>Pole Positions in 2023 Season: {polePositionsIn2023Season}</p>
+    <div>
+      {entities && entities.length > 0 ? (
+        entities.map(entity => (
+          <div className="entity" key={entity.id}>
+            <h2>{entity.name}</h2>
+            <p>Team: {entity.team}</p>
+            <p>Car Model: {entity.carModel}</p>
+            <p>Engine: {entity.engine}</p>
+            <p>Wins in 2023 Season: {entity.winsIn2023Season}</p>
+            <p>Pole Positions in 2023 Season: {entity.polePositionsIn2023Season}</p>
+          </div>
+        ))
+      ) : (
+        <p>No race cars available</p>
+      )}
     </div>
   );
 };
