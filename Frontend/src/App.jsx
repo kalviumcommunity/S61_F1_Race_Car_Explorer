e4953c01eab6f6a5156d6ba9c3bba3e5eb0ff0b0
Ishana0 <ishanakalikiri01@gmailcom>
2024-07-22 23:02:59 +0530

    cookie

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -5,6 +5,8 @@ import LandPage from './Components/LandPage';
 import Entity from './Components/Entity';
 import AddDataForm from './Components/AddDataForm';
 import UpdateDataForm from './Components/UpdateDataForm';
+import Login from './Components/Login';
+import Logout from './Components/Logout';
 
 function App() {
   return (
@@ -16,6 +18,8 @@ function App() {
             <Route path="/entities" element={<Entity />} />
             <Route path="/add" element={<AddDataForm />} />
             <Route path="/update" element={<UpdateDataForm />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/logout" element={<Logout />} />
           </Routes>
         </div>
       </>
